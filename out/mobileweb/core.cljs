4284ffc3574aa10ef3e14b5f4df53ecd70e1d1a1
jack <hongmomanu@163.com>
2015-10-12 16:12:09 +0800

    jack commit

--- out/mobileweb/core.cljs
+++ out/mobileweb/core.cljs
@@ -1,7 +1,10 @@
 (ns mobileweb.core
   (:require [goog.object :as o] [clojure.browser.repl :as repl])
-  (:use [jayq.core :only [$ css html]])
+  (:use [jayq.core :only [$ css html]]
+
+        )
   (:use-macros [purnam.core :only [obj arr ! def.n]]
+
                [gyr.core :only [def.module def.config def.factory
                                 def.controller def.service]])
   )
@@ -11,6 +14,9 @@
 (enable-console-print!)
 
 (println "Hello world!")
+
+
+(def serverurl "http://192.168.2.100:3000/")
 
 
 
@@ -31,10 +37,22 @@
 
   (println "AppCtrl")
   (.parse js/$.parser)
-  (! $scope.treedata  {})
+  (.treegrid ($ :#temptree) (obj :url (str serverurl "temp/gettemptree") :onContextMenu menushow  :method "get" ))
+  ;(! $scope.treedata  {})
 
   )
 
 (defn testctl  [$scope]
   (js/alert 33)
+  )
+
+(defn menushow [e node]
+
+  (js/console.log "haha")
+  (.preventDefault e)
+  ;(.tree ($ :#temptree) "select" node.target)
+
+  (.menu ($ :#mm) "show" (obj :left e.pageX :top e.pageY))
+
+  (js/console.log node)
   )
--- out/mobileweb/core.js
+++ out/mobileweb/core.js
@@ -6,18 +6,19 @@ goog.require('clojure.browser.repl');
 goog.require('jayq.core');
 cljs.core.enable_console_print_BANG_.call(null);
 cljs.core.println.call(null,"Hello world!");
+mobileweb.core.serverurl = "http://192.168.2.100:3000/";
 mobileweb.core.starter = angular.module("starter",["ngRoute","starter.controllers"]);
 angular.module("starter").config(["$routeProvider",(function ($routeProvider){
-return $routeProvider.when("/",(function (){var G__7733 = {};
-(G__7733["controller"] = "AppCtrl");
+return $routeProvider.when("/",(function (){var G__6531 = {};
+(G__6531["controller"] = "AppCtrl");
 
-(G__7733["templateUrl"] = "templates/main.html");
+(G__6531["templateUrl"] = "templates/main.html");
 
-return G__7733;
-})()).otherwise((function (){var G__7737 = {};
-(G__7737["redirectTo"] = "/");
+return G__6531;
+})()).otherwise((function (){var G__6535 = {};
+(G__6535["redirectTo"] = "/");
 
-return G__7737;
+return G__6535;
 })());
 })]);
 mobileweb.core.starter_controllers = angular.module("starter.controllers",[]);
@@ -26,15 +27,35 @@ cljs.core.println.call(null,"AppCtrl");
 
 ($["parser"]).parse();
 
-var o_SHARP_ = $scope;
-(o_SHARP_["treedata"] = cljs.core.PersistentArrayMap.EMPTY);
+return jayq.core.$.call(null,new cljs.core.Keyword(null,"#temptree","#temptree",-516572480)).treegrid((function (){var G__6539 = {};
+(G__6539["method"] = "get");
 
-return o_SHARP_;
+(G__6539["url"] = [cljs.core.str(mobileweb.core.serverurl),cljs.core.str("temp/gettemptree")].join(''));
+
+(G__6539["onContextMenu"] = mobileweb.core.menushow);
+
+return G__6539;
+})());
 })];
 
 angular.module("starter.controllers").controller("AppCtrl",mobileweb.core.starter_controllers_AppCtrl);
 mobileweb.core.testctl = (function mobileweb$core$testctl($scope){
 return alert((33));
+});
+mobileweb.core.menushow = (function mobileweb$core$menushow(e,node){
+console.log("haha");
+
+e.preventDefault();
+
+jayq.core.$.call(null,new cljs.core.Keyword(null,"#mm","#mm",-219537863)).menu("show",(function (){var G__6547 = {};
+(G__6547["top"] = (e["pageY"]));
+
+(G__6547["left"] = (e["pageX"]));
+
+return G__6547;
+})());
+
+return console.log(node);
 });
 
 //# sourceMappingURL=core.js.map
